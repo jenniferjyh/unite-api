5dd97c98af959218a0eeb9d44cd8cd4686449327
Jennifer Yuheng <38702680+jenniferjyh@users.noreply.github.com>
2020-05-16 12:35:37 -0700

    please

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://unite-app.surge.sh/"],
+    origin: ["http://unite-app.surge.sh", "http://unite-app.surge.sh"],
   })
 );
 
